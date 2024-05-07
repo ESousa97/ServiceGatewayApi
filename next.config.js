6231cb59e5d96660cfbba99c6895ff924bd51881
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 10:34:11 -0300

    test 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-// next.config.js
 module.exports = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'production' ? 'https://service-gateway-api.vercel.app//api/:path*' : 'http://localhost:5001/:path*',
+        destination: process.env.NODE_ENV === 'production' ? 'https://service-gateway-api.vercel.app/api/:path*' : 'http://localhost:5001/:path*',
       },
       {
         source: '/cluster-tickets',
