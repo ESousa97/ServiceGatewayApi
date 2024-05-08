88d4f1ab5f04bd1e53e6ca1c95542d04bff1146f
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-08 10:17:23 -0300

    att correção

--- api/app.py
+++ api/app.py
@@ -1,22 +1,16 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify
 import requests
 import logging
 from flask_cors import CORS
-from werkzeug.urls import url_encode
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 CORS(app, origins='*')
 
 @app.route('/cluster-tickets', methods=['POST'])
 def cluster_tickets():
-    data = request.get_json()
-    if not data or 'tickets' not in data or not isinstance(data['tickets'], list):
-        logging.error("Invalid data format: JSON body must be a list of ticket dictionaries.")
-        abort(400, description="Bad Request: JSON body must be a list of ticket dictionaries.")
-    
-    tickets = data['tickets']
+    tickets = request.get_json()
     descriptions = [
         " ".join([
             ticket.get('cause_by', ''),
@@ -24,45 +18,43 @@ def cluster_tickets():
             ticket.get('solution_by', ''),
             ticket.get('validated_by', '')
         ]).strip()
-        for ticket in tickets if isinstance(ticket, dict)
+        for ticket in tickets
     ]
 
-    logging.info("Descriptions collected for normalization: %s", descriptions)
+    logging.debug(f"Descriptions: {descriptions}")
 
     try:
         normalized_response = requests.post('https://service-standardization-text-api.vercel.app/normalize', json={"texts": descriptions})
         normalized_response.raise_for_status()
         normalized_texts = normalized_response.json()
-        logging.info("Normalized Texts received: %s", normalized_texts)
+        logging.debug(f"Normalized Texts: {normalized_texts}")
     except requests.exceptions.RequestException as e:
-        logging.error("Error with normalization service: %s", e, exc_info=True)
+        logging.error("Error with normalization service", exc_info=True)
         return jsonify({"error": str(e)}), 500
 
     try:
-        logging.info("Sending descriptions to vectorization service.")
         vector_response = requests.post('https://service-vectorization-api.vercel.app/vectorize', json={"texts": normalized_texts})
         vector_response.raise_for_status()
         vectors = vector_response.json()
-        logging.info("Vectors received from vectorization service: %s", vectors)
+        logging.debug(f"Vectors: {vectors}")
     except requests.exceptions.RequestException as e:
-        logging.error("Error with vectorization service: %s", e, exc_info=True)
+        logging.error("Error with vectorization service", exc_info=True)
         return jsonify({"error": str(e)}), 500
 
     try:
-        logging.info("Sending vectors to clustering service.")
         cluster_response = requests.post('https://service-group-api.vercel.app/cluster', json={"vectors": vectors})
         cluster_response.raise_for_status()
         labels = cluster_response.json()
-        logging.info("Labels received from clustering service: %s", labels)
+        logging.debug(f"Labels: {labels}")
     except requests.exceptions.RequestException as e:
-        logging.error("Error with clustering service: %s", e, exc_info=True)
+        logging.error("Error with clustering service", exc_info=True)
         return jsonify({"error": str(e)}), 500
 
     clusters = {}
     for idx, label in enumerate(labels):
         clusters.setdefault(label, []).append(tickets[idx])
 
-    logging.info("Final clusters formed: %s", clusters)
+    logging.debug(f"Clusters: {clusters}")
     return jsonify(clusters)
 
 if __name__ == '__main__':
